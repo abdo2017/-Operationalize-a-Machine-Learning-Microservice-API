0925332e83ce7ea79718dd531ddd3eda6cd1f179
Ubuntu <ubuntu@ip-172-31-31-120.ec2.internal>
2021-12-04 07:12:55 +0000

    start

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -6,13 +6,15 @@
 # This is your Docker ID/path
 # dockerpath=<>
 
+dockerpath="abdoesam2011/abdo"
+
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run abdoservice --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward abdoservice 8000:80 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+
+dockerpath="abdoesam2011/abdo"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag abdo $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
